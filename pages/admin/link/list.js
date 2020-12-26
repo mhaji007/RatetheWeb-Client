09a31712826517fae08efe1cd845bc1e708d335f
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-12-26 12:29:32 -0500

    Implement link delete functionality for admin

--- pages/admin/link/list.js
+++ pages/admin/link/list.js
@@ -24,6 +24,39 @@ function Links({ links, totalLinks, linksLimit, linkSkip, token }) {
   // is a need for further request
   const [size, setSize] = useState(totalLinks);
 
+
+  const confirmDelete = (e, id) => {
+    e.preventDefault();
+    // console.log('delete > ', slug);
+    let answer = window.confirm("Are you sure you want to delete this link?");
+    if (answer) {
+      handleDelete(id);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    console.log("delete link", id);
+    try {
+      const response = await axios.delete(
+        `${process.env.NEXT_PUBLIC_API}/link/admin/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("Link delete success ", response);
+      // Since this page is for admin(s) only.
+      // instead of refetching all links, reload the page
+      // and refetch the links
+      process.browser && window.location.reload()
+    } catch (error) {
+      console.log("Link delete ", error);
+    }
+  };
+
+
+
   const listOfLinks = () =>
     allLinks.map((l, i) => (
       <div
@@ -43,7 +76,7 @@ function Links({ links, totalLinks, linksLimit, linkSkip, token }) {
         </div>
         {/* Display link meta information */}
         <div className="col-md-4 pt-2">
-          <span className="float-right">
+          <span className="float-right text-nowrap mb-1">
             {moment(l.createdAt).fromNow()} by {l.postedBy.name}
           </span>
         </div>
@@ -63,8 +96,9 @@ function Links({ links, totalLinks, linksLimit, linkSkip, token }) {
               {c.name}
             </span>
           ))}
+
           <span className="badge badge-pill badge-primary bg-primary ml-1 float-right ">
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
@@ -73,8 +107,20 @@ function Links({ links, totalLinks, linksLimit, linkSkip, token }) {
               viewBox="0 0 16 16"
             >
               <path d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z" />
-            </svg>{" "}
+            </svg>{" "} */}
             {l.clicks ? l.clicks : 0} clicks
+          </span>
+          <Link href={`/admin/link/${l._id}`}>
+            <span className="badge border border-info text-info float-right ml-2 ">
+              Update
+            </span>
+          </Link>
+
+          <span
+            onClick={(e) => confirmDelete(e, l._id)}
+            className="badge border border-danger text-danger float-right"
+          >
+            Delete
           </span>
         </div>
       </div>
@@ -85,7 +131,7 @@ function Links({ links, totalLinks, linksLimit, linkSkip, token }) {
     let toSkip = skip + limit;
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_API}/links`,
-      { skip, limit },
+      { skip:toSkip, limit },
       { headers: { Authorization: `Bearer ${token}` } }
     );
     // Add new links returned from request while preserving the existing links
